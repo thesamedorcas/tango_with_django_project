720f9b62eef2d9d000e795cc306ff8cb5f0cbd25
Dorcas Adetunji <2951225a@student.gla.ac.uk>
2025-01-24 08:34:55 +0000

    Exercise- add about page

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,8 @@ from rango import views
 app_name = 'rango'  
 
 urlpatterns = [
-    path('', views.index, name='index'),  
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'), 
+    
+
 ]
