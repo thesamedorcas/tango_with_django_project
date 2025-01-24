42737ed9cc638776ffce284997f67bfd01d858bf
Dorcas Adetunji <2951225a@student.gla.ac.uk>
2025-01-24 10:59:37 +0000

    Added template

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
